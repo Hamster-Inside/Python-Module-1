d1094ad3cd102632d8561906141aee4349566c7f
Arek <abudkowski@interia.pl>
2022-04-05 06:55:30 +0200

    added exceptions, deleted infinite loop of game

--- main.py
+++ main.py
@@ -27,6 +27,18 @@ split cards
 """
 
 
+class GameLostException(Exception):
+    pass
+
+
+class WinGameException(Exception):
+    pass
+
+
+class DrawException(Exception):
+    pass
+
+
 class PlayerStatus(Enum):
     PLAYING = 0
     WIN = 1
@@ -118,28 +130,33 @@ class Game:
         player_to_count_values_from.count = 0
         for card_val in self.deck.deck_cards[player_to_count_values_from]:
             player.count += self.deck.cards.card_value(card_val)
+        if player.count > 21:
+            player.status = PlayerStatus.LOST
+            raise GameLostException(f'Player {player} LOST, value over 21')
 
 
 game = Game(num_of_decks=3)
 game.add_player('Boguś')
 game.add_player('Homar')
 game.start_deck_game()  # give 2 cards to every player, where croupier has one hidden
-while True:
-    for player in game.player_list:
-        while True:
-            game.view_deck()
-            operation = input(f"{player} - What do you want to do? (draw, pass, split)\n")
-            match operation.lower():
-                case 'draw':
+
+for player in game.player_list:
+    while True:
+        game.view_deck()
+        operation = input(f"{player} - What do you want to do? (draw, pass, split)\n")
+        match operation.lower():
+            case 'draw':
+                try:
                     game.draw_card(player)
-                case 'pass':
+                    game.count_player_cards_value(player)
+                except GameLostException as e:
+                    print(e)
                     break
-                case 'split':
-                    game.split_cards(player)
-                case _:
-                    print('Need to Draw, Pass or Split')
-            game.count_player_cards_value(player)
-            if player.count > 21:
-                player.status = PlayerStatus.LOST
+            case 'pass':
                 break
+            case 'split':
+                game.split_cards(player)
+            case _:
+                print('Need to Draw, Pass or Split')
+game.view_deck()
 
